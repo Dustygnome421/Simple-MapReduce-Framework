d82d166fe8e6cee640d10f01cb0f48d489b600d5
Aman Gupta <41102741+Dustygnome421@users.noreply.github.com>
2023-04-22 13:05:51 -0400

    Phase 3 Done

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,5 +1,7 @@
 package mapreduce
 
+import "sync"
+
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
@@ -11,6 +13,9 @@ func (mr *Master) schedule(phase jobPhase) {
 	case reducePhase:
 		ntasks = mr.nReduce
 		nios = len(mr.files)
+		// for i := 0; i < len(mr.workers); i++ {
+		// 	mr.doneChannel <- true
+		// }
 	}
 
 	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
@@ -22,25 +27,27 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO:
 
-	registerChan := make(chan string)
+	// ---------------------------
 
-	// Create a channel for each worker
-	go func ()  {
-		for {
-			worker := <-mr.registerChannel
-			registerChan <- worker
-		}
-	}()
-
+	// Buffered Channel to store the task numbers
 	taskChannel := make(chan int, ntasks)
 	for i := 0; i < ntasks; i++ {
 		taskChannel <- i
 	}
 
-	for len(taskChannel) > 0 {
+	// WaitGroup to wait for all tasks to be completed
+	var wg sync.WaitGroup
+	wg.Add(ntasks)
+
+	// Loop through all tasks
+	for i := 0 ; i < ntasks; i++ {
+		// Get the task number from the channel
 		task := <-taskChannel
+		// Start a goroutine to do the task
 		go func (taskNumber int, phase jobPhase, nios int)  {
-			worker := <- registerChan
+			// Get a worker from the register channel
+			worker := <- mr.registerChannel
+			// Create the arguments for the DoTask RPC
 			args := &DoTaskArgs{
 				JobName:       mr.jobName,
 				File:          mr.files[taskNumber],
@@ -48,20 +55,40 @@ func (mr *Master) schedule(phase jobPhase) {
 				TaskNumber:    taskNumber,
 				NumOtherPhase: nios,
 			}
-			ok := call(worker, "Worker.DoTask", args, new(struct{}))
+			// Call the DoTask RPC
+			ok := call(worker, "Worker.DoTask", args, new(ShutdownReply))
+			
+			// If the RPC was successful, put the worker back in the register channel
+			// Else, put the task number back in the task channel
 			if ok {
-				registerChan <- worker
-				<- taskChannel
+				go func() { 
+					// Decrement the wait group
+					wg.Done()
+					mr.registerChannel <- worker 
+				}()
 			} else {
 				taskChannel <- taskNumber
 			}
 		}(task, phase, nios)
 	}
 
-	<- taskChannel
-
+	// Wait for all tasks to be completed
+	wg.Wait()
 
 	// ---------------------------
 
 	debug("Schedule: %v phase done\n", phase)
+}
+
+
+func getNextTask(jobName string, file []string, taskNumber int, phase jobPhase, nios int) (task DoTaskArgs){
+	
+	task = DoTaskArgs{
+		JobName:       jobName,
+		File:          file[taskNumber],
+		Phase:         phase,
+		TaskNumber:    taskNumber,
+		NumOtherPhase: nios,
+	}
+	return task
 }
